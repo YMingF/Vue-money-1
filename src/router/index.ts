2879ec5e9a5d61665571f142927dce8d502b0630
YM <a834319313@163.con>
2021-09-15 12:30:55 +0800

    全局引入<Nav/>

---
+++ src/router/index.ts
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import VueRouter, { RouteConfig } from 'vue-router'
+import Home from '@/views/Home.vue'
+import Money from "@/views/Money.vue"
+import Labels from "@/views/Labels.vue"
+import Statistics from "@/views/Statistics.vue"
+Vue.use(VueRouter)
+
+const routes = [
+  {//设置默认路径
+    path:'/', 
+    redirect:'/money',//重定向到money
+  },
+  {
+    path:'/money',
+    component:Money
+  },
+  {
+    path:'/labels',
+    component:Labels
+  },
+  {
+    path:'/statistics',
+    component:Statistics
+  }
+ 
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
