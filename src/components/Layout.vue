670a8eb842400befa9fb416ae68384e71b334c23
YM <a834319313@163.con>
2021-09-15 18:50:05 +0800

    封装Icon.vue

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -9,13 +9,12 @@
 
 <script>
     export default {
-        
+        name: 'Layout'
     }
 </script>
 
 <style lang="scss" scoped>
 .nav-wrapper{
-     border: 1px solid green;
      display: flex;
      flex-direction: column;
      height: 100vh;
@@ -24,5 +23,7 @@
      flex-grow: 1; //高度尽可能分配给.content剩余的给别人
      overflow: auto;
 
+
  }
+
 </style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,29 +1,35 @@
 <template>
     <div class="nav">
         <router-link to='/money'>
-            <svg>  
-                <use href='#label' />
-            </svg>
+             <Icon name='money'/>
             记账
         
         </router-link>
         |
-        <router-link to='/labels'>标签</router-link>
+        <router-link to='/labels'>
+            <Icon name='label'/>
+            标签
+        </router-link>
         |
-        <router-link to='/statistics'>统计</router-link>
+        <router-link to='/statistics'>
+            <Icon name='statistics'/>
+            统计
+        </router-link>
     </div>
 </template>
-
-<script>
-    import x from '@/assets/icons/label.svg'
-     import y from '@/assets/icons/money.svg'
+<script lang='ts'>
+    
+    
     export default {
-        
+         name: 'Nav'
     }
 </script>
 
 <style lang="scss" scoped>
 .nav{
     border: 1px solid  red ;
+    display: flex;
+    flex-direction: row;
+    font-size: 12px;
 }
 </style>
