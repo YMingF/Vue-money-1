cabeb834f061aa43625579721f065b5cf909f9ed
YM <a834319313@163.con>
2021-09-15 16:22:04 +0800

    导入svg

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="nav">
-        <router-link to='/money'>记账</router-link>
+        <router-link to='/money'>
+            <svg>  
+                <use href='#label' />
+            </svg>
+            记账
+        
+        </router-link>
         |
         <router-link to='/labels'>标签</router-link>
         |
@@ -9,6 +15,8 @@
 </template>
 
 <script>
+    import x from '@/assets/icons/label.svg'
+     import y from '@/assets/icons/money.svg'
     export default {
         
     }
