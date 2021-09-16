891cce1f39645ec2eeda9927944b0dd2ed4289a0
YM <a834319313@163.con>
2021-09-16 19:41:46 +0800

    完成Money组件的HTML框架

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -7,6 +7,7 @@
 <script lang='ts'>
     let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
     try {importAll(require.context('../assets/icons', true, /\.svg$/));} catch (error) {console.log(error);}
+
     export default {
         name:"Icon",
         props:['name'] //外部属性
