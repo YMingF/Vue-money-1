d45d2be7b9cce0d785fd4415f7eff0b5c0849a0e
YM <a834319313@163.con>
2021-09-17 13:56:33 +0800

    5-6Money.vue的CSS调整完毕

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,29 +1,28 @@
 <template>
-   <div class="nav-wrapper">
-        <div class="content">
-            <slot />  <!--插槽，用于接收等下给组件传入的东西-->
-        </div>
-        <Nav />
+  <div class="layout-wrapper" :class="classPrefix && `${classPrefix}-wrapper`">
+    <div class="content" :class="classPrefix && `${classPrefix}-content`">
+      <slot />
+      <!--插槽，用于接收等下给组件传入的东西-->
     </div>
+    <Nav />
+  </div>
 </template>
 
-<script lang='ts'>
-    export default {
-        name: 'Layout'
-    }
+<script lang="ts">
+export default {
+  name: "Layout",
+  props: ["classPrefix"],
+};
 </script>
 
 <style lang="scss" scoped>
-.nav-wrapper{
-     display: flex;
-     flex-direction: column;
-     height: 100vh;
- }
- .content{
-     flex-grow: 1; //高度尽可能分配给.content剩余的给别人
-     overflow: auto;
-
-
- }
-
+.layout-wrapper {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content {
+  flex-grow: 1; //高度尽可能分配给.content剩余的给别人
+  overflow: auto;
+}
 </style>
