c80aa031cfaea9ea002d7f19c65f0daab718b1aa
YM <a834319313@163.con>
2021-09-22 18:34:57 +0800

    7-3:使用LocalStorage存储改变的数据

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout classPrefix="layout">
-    {{ record }}
-    <NumberPad @update:value="onUpdateAmount" />
+    {{ recordList }}
+    <NumberPad @update:value="onUpdateAmount" @submit="saveRecord" />
     <!--支出收入-->
     <Types :value.sync="record.type" />
     <!--备注-->
@@ -18,24 +18,27 @@ import NumberPad from "@/components/Money/NumberPad.vue";
 import Types from "@/components/Money/Types.vue";
 import Notes from "@/components/Money/Notes.vue";
 import Tags from "@/components/Money/Tags.vue";
-import { Component } from "vue-property-decorator";
-
+import { Component, Watch } from "vue-property-decorator";
 type Record = {
   //对象的类型声明
   tags: string[];
   notes: string;
   type: string;
-  amount: number;
+  amount: number; //数据类型
+  createdAt?: Date; //类
 };
 @Component({
   components: { Types, NumberPad, Notes, Tags },
 })
 export default class Money extends Vue {
   tags = ["衣", "食", "住", "行"];
+  recordList: Record[] = JSON.parse(
+    window.localStorage.getItem("recordList") || "[]"
+  ); //存储提交的记录
   record: Record = {
     tags: [],
     notes: "",
-    type: "-",
+    type: "+",
     amount: 0,
   };
   onUpdateTags(value: string[]) {
@@ -47,6 +50,16 @@ export default class Money extends Vue {
 
   onUpdateAmount(value: string) {
     this.record.amount = parseFloat(value);
+  }
+  saveRecord() {
+    const record2: Record = JSON.parse(JSON.stringify(this.record));
+    record2.createdAt = new Date(); //存储当前时间
+    this.recordList.push(record2);
+    console.log(this.recordList);
+  }
+  @Watch("recordList")
+  onRecordListChange() {
+    window.localStorage.setItem("recordList", JSON.stringify(this.recordList));
   }
 }
 </script>
