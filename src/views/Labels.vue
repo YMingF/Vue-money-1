c2bb47ef5efc059885f59aa5b428ddd82cb0a833
YM <a834319313@163.con>
2021-09-23 16:05:58 +0800

    8-4:Label.vue的css的设置

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -18,8 +18,8 @@
         <Icon name="right" />
       </li>
     </ol>
-    <div>
-      <button>新建标签</button>
+    <div class="createTag-wrapper">
+      <button class="createTag">新建标签</button>
     </div>
   </Layout>
 </template>
@@ -33,7 +33,33 @@ export default {
 <style lang="scss" scoped>
 .tags {
   background-color: white;
+  font-size: 16px;
+  padding-left: 16px;
   > li {
+    min-height: 44px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between; //实现文字和箭头两边显示
+    border-bottom: 1px solid #e6e6e6;
+    svg {
+      width: 18px;
+      height: 18px;
+      color: #666;
+      margin-right: 16px;
+    }
+  }
+}
+.createTag {
+  background-color: #767676;
+  color: white;
+  border-radius: 4px;
+  border: none;
+  height: 40px;
+  padding: 0 16px;
+  &-wrapper {
+    text-align: center;
+    padding: 16px;
+    margin-top: 44-16px;
   }
 }
 </style>
