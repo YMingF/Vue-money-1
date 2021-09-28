a9102532bdc42a9c69d876426694f7ff78b9f3ee
YM <a834319313@163.con>
2021-09-28 14:33:30 +0800

    8-10:在上个版本的基础上删掉了一些无用代码&&保存ID到localStorage

--- src/lib/createId.ts
+++ src/lib/createId.ts
@@ -1,6 +1,7 @@
 let id:number = parseInt(window.localStorage.getItem("_idMax")|| '0')||0 ; //前一个||'0'是保证parseInt里有一个字符串,后一个||0是保证最终结果最起码是个0，而不会是NaN
 function createId() {
   id++;
+  window.localStorage.setItem('_idMax',id.toString())
   return id;
 }
 
