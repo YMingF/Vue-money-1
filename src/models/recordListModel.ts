217ec1870cf431ba14387aced585558a777a918e
YM <a834319313@163.con>
2021-09-28 15:03:12 +0800

    9-1:操作2:将create封装到recordListModel&&定义一个clone.ts存储常用的深拷贝代码

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,10 +1,16 @@
 
 import { RecordItem } from "@/custom";
+import clone from "@/lib/clone";
 const localStorageKeyName = "recordList";
 const recordListModel = {
   data:[] as RecordItem[],//单个数据的类型定义可以用as强制进行类型断言
   clone(data:RecordItem[]|RecordItem){ //深拷贝的代码,都写到这里是为了Money.vue里不出现JSON相关的内容
     return JSON.parse(JSON.stringify(data))
+  },
+  create(record:RecordItem){
+    const record2: RecordItem = clone(record);
+    record2.createdAt = new Date(); //存储当前时间
+    this.data.push(record2);
   },
   fetch() {
     this.data=JSON.parse(window.localStorage.getItem(localStorageKeyName) || "[]") as RecordItem[];//说清楚返回值到底是啥类型
