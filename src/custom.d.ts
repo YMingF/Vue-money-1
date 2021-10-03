a0346c4600da230fcf7de6b212ff087f9931c103
YM <a834319313@163.con>
2021-10-03 16:45:31 +0800

    解决小bug，调整了标签重复报错时的逻辑

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,6 +1,7 @@
 type RootState={
   recordList:  RecordItem[],
   createRecordError:Error |null, //用来等下存储创建数据时的错误
+  createTagError:Error|null,
   tagList:  Tag[],
   currentTag?:Tag
 }
--- src/mixins/TagHelper.ts
+++ src/mixins/TagHelper.ts
@@ -1,7 +1,11 @@
     
-// mixins.js
 import Vue from 'vue'
 import Component from 'vue-class-component'
+
+//定义对象任意字符名，避免检查为空报错
+const map:{[key:string]:string}={
+  'tag name duplicated':'标签名重复'
+}
 
 // You can declare mixins as the same style as components.
 @Component
@@ -12,6 +16,9 @@ export class TagHelper extends Vue {
       return window.alert('标签名不能为空')
     }
     this.$store.commit('createTag', name)
+    if(this.$store.state.createTagError){
+      window.alert(map[this.$store.state.createTagError.message]|| '未知错误') //获取到错误信息并警示出来
+    }
   }
 }
 
