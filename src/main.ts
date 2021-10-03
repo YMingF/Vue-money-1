753e757035e7e003ae046e1e63e13f89cf1ca0b7
YM <a834319313@163.con>
2021-10-03 19:09:33 +0800

    让界面中心出现二维码，确保用户能扫码用手机预览

--- src/main.ts
+++ src/main.ts
@@ -24,3 +24,13 @@ window.onload=function(){
     window.scrollTo(0,1000)
   },0)
 }
+if(document.documentElement.clientWidth>500){
+  const img=document.createElement('img')
+  img.src='/qrcode.png';
+  img.style.position='fixed';
+  img.style.left='50%';
+  img.style.top='50%';
+  img.style.transform='translate(-50%,-50%)';
+  img.style.boxShadow='0 0 10px rgba(0,0,0,0.25)';
+  document.body.appendChild(img)
+}
