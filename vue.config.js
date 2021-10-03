b4591de83c5ee891cc237393109ed3923bd4a8d7
YM <a834319313@163.con>
2021-10-03 18:54:32 +0800

    部署网站到money-website

--- vue.config.js
+++ vue.config.js
@@ -1,30 +1,29 @@
-const path = require("path");
+const path = require('path')
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/money-website/' : '/',
   lintOnSave: false,
-  chainWebpack: (config) => {
-    const dir = path.resolve(__dirname, "src/assets/icons");
+  chainWebpack: config => {
+    const dir = path.resolve(__dirname, 'src/assets/icons')
 
     config.module
-      .rule("svg-sprite")
+      .rule('svg-sprite')
       .test(/\.svg$/)
       .include.add(dir)
       .end() // 只包含 icons 目录
-      .use("svg-sprite-loader")
-      .loader("svg-sprite-loader")
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
       .options({ extract: false })
       .end()
-      .use("svgo-loader")
-      .loader("svgo-loader")
-      .tap((options) => ({
+      .use('svgo-loader')
+      .loader('svgo-loader')
+      .tap(options => ({
         ...options,
-        plugins: [{ removeAttrs: { attrs: "fill" } }],
+        plugins: [{ removeAttrs: { attrs: 'fill' } }]
       }))
-      .end();
-    config
-      .plugin("svg-sprite")
-      .use(require("svg-sprite-loader/plugin"), [{ plainSprite: true }]);
-    config.module.rule("svg").exclude.add(dir); // 其他 svg loader 排除 icons 目录
+      .end()
+    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{ plainSprite: true }])
+    config.module.rule('svg').exclude.add(dir) // 其他 svg loader 排除 icons 目录
 
     // config.module
     //   .rule('svg-sprite')
@@ -36,5 +35,5 @@ module.exports = {
     //   .end()
     // config.plugin('svg-sprite').use(require('svg-sprite-loader-mod/plugin'), [{plainSprite: true}])
     // config.module.rule('svg').exclude.add(dir)
-  },
-};
+  }
+}
