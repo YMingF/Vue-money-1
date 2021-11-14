2a200246ab1423854f7e8fe4fd6ff77da943f66c
YMingF <834319313@qq.com>
2021-11-14 11:29:44 +0800

    自己封装echarts组件

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,17 +1,18 @@
 <template>
   <Layout>
-    <Tabs classPrefix="type" :dataSource="recordTypeList" :value.sync="type" />
+    <Tabs classPrefix="type" :dataSource="recordTypeList" :value.sync="type"/>
+    <Chart :options="option"/>
     <ol v-if="groupedList.length>0">
       <li v-for="(group,index) in groupedList" :key="index">
         <h3 class="title">
-          {{beautify(group.title)}}
-          <span>￥{{group.total}}</span>
+          {{ beautify(group.title) }}
+          <span>￥{{ group.total }}</span>
         </h3>
         <ol>
           <li v-for="item in group.items" :key="item.id" class="record">
-            <span>{{tagString(item.tags)}}</span>
-            <span class="notes">{{item.notes}}</span>
-            <span>￥{{item.amount}}</span>
+            <span>{{ tagString(item.tags) }}</span>
+            <span class="notes">{{ item.notes }}</span>
+            <span>￥{{ item.amount }}</span>
           </li>
         </ol>
       </li>
@@ -26,6 +27,7 @@
   padding: 16px;
   text-align: center;
 }
+
 %item {
   padding: 8px 16px;
   line-height: 24px;
@@ -33,18 +35,22 @@
   justify-content: space-between;
   align-items: center;
 }
+
 .title {
   @extend %item;
 }
+
 .record {
   background-color: white;
   @extend %item;
 }
+
 .notes {
   margin-right: auto; //让备注能够靠左显示，而不会居中
   margin-left: 16px;
   color: #999;
 }
+
 ::v-deep {
   .type-tabs-item {
     background-color: #c4c4c4;
@@ -57,80 +63,123 @@
       }
     }
   }
+
   .interval-tabs-item {
     height: 48px;
   }
 }
+
+.chart {
+  height: 400px;
+}
 </style>
 
 <script lang='ts'>
-import Vue from 'vue'
 
-import Tabs from '@/components/Tabs.vue'
-import recordTypeList from '@/constants/recordTypeList'
-import { Component } from 'vue-property-decorator'
-import dayjs from 'dayjs'
-import clone from '@/lib/clone'
+import Vue from 'vue';
+import Tabs from '@/components/Tabs.vue';
+import recordTypeList from '@/constants/recordTypeList';
+import {Component} from 'vue-property-decorator';
+import dayjs from 'dayjs';
+import clone from '@/lib/clone';
+import Chart from '@/components/Chart.vue';
+
 @Component({
-  components: { Tabs }
+  components: {Tabs, Chart}
 })
 export default class Statistics extends Vue {
-  type = '-'
-  recordTypeList = recordTypeList
+  type = '-';
+  recordTypeList = recordTypeList;
+
   tagString(tags: Tag[]) {
-    return tags.length === 0 ? '无' : tags.map(t => t.name).join('，')
+    return tags.length === 0 ? '无' : tags.map(t => t.name).join('，');
   }
+
   beautify(string: string) {
-    const day = dayjs(string)
-    const now = dayjs() //获取到当前时间
+    const day = dayjs(string);
+    const now = dayjs(); //获取到当前时间
     if (day.isSame(now, 'day')) {
-      return '今天'
+      return '今天';
     } else if (day.isSame(now.subtract(1, 'day'), 'day')) {
-      return '昨天'
+      return '昨天';
     } else if (day.isSame(now.subtract(2, 'day'), 'day')) {
-      return '前天'
+      return '前天';
     } else if (day.isSame(now, 'year')) {
       //如果是同一年，就省掉年份的显示
-      return day.format('M月-D日')
+      return day.format('M月-D日');
     } else {
-      return day.format('YYYY年-M月-D日')
+      return day.format('YYYY年-M月-D日');
     }
   }
-  get recordList() {
-    return (this.$store.state as RootState).recordList
+
+  get option() {
+    return {
+      tooltip: {show: true},
+      xAxis: {
+        type: 'category',
+        data: [
+          '1', '2', '3', '4', '5', '6', '7',
+          '8', '9', '10', '11', '12', '13', '14',
+          '15', '16', '17', '18', '19', '20', '21',
+          '22', '23', '24', '25', '26', '27', '28',
+          '29', '30',
+        ]
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [
+            120, 200, 150, 80, 70, 110, 130,
+            120, 200, 150, 80, 70, 110, 130,
+            120, 200, 150, 80, 70, 110, 130,
+            120, 200, 150, 80, 70, 110, 130,
+            120, 200
+          ],
+          type: 'line'
+        }
+      ]
+    };
   }
+
+  get recordList() {
+    return (this.$store.state as RootState).recordList;
+  }
+
   get groupedList() {
-    const { recordList } = this //等同于recordList=this.recordList
+    const {recordList} = this; //等同于recordList=this.recordList
 
     type Result = { title: string; total?: number; items: RecordItem[] }[]
 
     const newList = clone(recordList)
-      .filter(r => r.type === this.type)
-      .sort((a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf())
+        .filter(r => r.type === this.type)
+        .sort((a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf());
     //加分支解决没有收入数据时的undefined报错
     if (newList.length === 0) {
-      return [] as Result
+      return [] as Result;
     }
 
-    const result: Result = [{ title: dayjs(newList[0].createdAt).format('YYYY-MM-DD'), items: [newList[0]] }]
+    const result: Result = [{title: dayjs(newList[0].createdAt).format('YYYY-MM-DD'), items: [newList[0]]}];
     for (let i = 1; i < newList.length; i++) {
-      const current = newList[i]
-      const last = result[result.length - 1]
+      const current = newList[i];
+      const last = result[result.length - 1];
 
       if (dayjs(last.title).isSame(dayjs(current.createdAt), 'day')) {
-        last.items.push(current)
+        last.items.push(current);
       } else {
-        result.push({ title: dayjs(current.createdAt).format('YYYY-M-D'), items: [current] })
+        result.push({title: dayjs(current.createdAt).format('YYYY-M-D'), items: [current]});
       }
     }
     //取出列表里的每一项，然后将统计到的总金额设置到total属性
     result.map(group => {
-      group.total = group.items.reduce((sum, item) => sum + item.amount, 0)
-    })
-    return result
+      group.total = group.items.reduce((sum, item) => sum + item.amount, 0);
+    });
+    return result;
   }
+
   beforeCreate() {
-    this.$store.commit('fetchRecords')
+    this.$store.commit('fetchRecords');
   }
 }
 </script>
